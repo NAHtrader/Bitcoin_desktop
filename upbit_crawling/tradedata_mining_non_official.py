8ead2336fa26d240c6bd01d3d0793688ca0dade9
nahtrader <nate05@naver.com>
2021-05-17 20:05:24 +0900

    bolinger band

--- upbit_crawling/tradedata_mining_non_official.py
+++ upbit_crawling/tradedata_mining_non_official.py
@@ -19,34 +19,40 @@ def call_coin():
     return databox
 
 # months=['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+# num=[]
+# for i in range(1,32):
+#     a=''+str(i)+''
+#     num.append(a)
+# print(num)
 
-
+days=['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
+#
 coin_names=call_coin()
 # cycle=0
 for coin in coin_names:
     box = {}
+    for day in days:
+        r = requests.get("https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/15?code=CRIX.UPBIT."+coin[0]+"&count=96&to=2021-03-"+day+"%2009:00:00")
+        tradedata = r.json()
 
-    r = requests.get("https://crix-api-endpoint.upbit.com/v1/crix/candles/days?code=CRIX.UPBIT."+coin[0]+"&count=90&to=2021-03-31%2009:00:00")
-    tradedata = r.json()
-
-    for trade in reversed(tradedata):
-        time = trade['candleDateTime']
-        timedata = time[0:10]
-        op = trade['openingPrice']  # op 는 시작 가격
-        tp = trade['tradePrice']  # tp 는 종가
-        lp = trade['lowPrice']  # lp 는 저가
-        hp = trade['highPrice']  # hp 는 고가
-        volume=trade['candleAccTradeVolume']
-        price=trade['candleAccTradePrice']
-        tap=price/volume
-        box[timedata] = [op,hp,lp,tp,volume,price,tap]
+        for trade in reversed(tradedata):
+            time = trade['candleDateTime']
+            timedata = time[0:10] +" "+ time[11:19]
+            op = trade['openingPrice']  # op 는 시작 가격
+            tp = trade['tradePrice']  # tp 는 종가
+            lp = trade['lowPrice']  # lp 는 저가
+            hp = trade['highPrice']  # hp 는 고가
+            volume=trade['candleAccTradeVolume']
+            price=trade['candleAccTradePrice']
+            tap=price/volume
+            box[timedata] = [op,hp,lp,tp,volume,price,tap]
 
     tradedata = pd.DataFrame(box)
     tradedata.rename(index={0: "Open Price", 1: "High Price", 2: "Low Price", 3: 'Close Price',4:'Trade Volume',5:'Total trade-price',6:'Average Trade price'}, inplace=True)
     transposed_tradedata = tradedata.transpose()
-    transposed_tradedata.to_excel(excel_writer='C:/Users/whdtlr/Bitcoin_desktop/2021/'+coin[1]+'.xlsx')
+    transposed_tradedata.to_excel(excel_writer='C:/Users/whdtlr/Bitcoin_desktop/march_15m/'+coin[1]+'.xlsx')
     print(coin[1])
-    # cycle+=1
-    # if cycle%5==0:
-    #     sleep(60)
-    #     print("-------waiting-------")
+#     # # cycle+=1
+#     # # if cycle%5==0:
+#     # #     sleep(60)
+#     # #     print("-------waiting-------")
